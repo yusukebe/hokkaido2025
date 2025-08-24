efa4dd636ffa91abaddfec4452a4687716f3b63a
Yusuke Wada <yusuke@kamawada.com>
2025-08-25 05:42:10 +0900

    add `react-vite` project

---
+++ projects/react-vite/src/server/index.tsx
@@ -0,0 +1,12 @@
+import { Hono } from 'hono'
+import { renderer } from './renderer'
+
+const app = new Hono()
+
+app.use(renderer)
+
+app.get('/', (c) => {
+  return c.render(<div id="root"></div>)
+})
+
+export default app
