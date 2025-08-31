f8fa3524a8aa747b074eb4393651468ddb680926
Yusuke Wada <yusuke@kamawada.com>
2025-08-31 21:08:02 +0900

    remove the unnecessay function

--- projects/ramen-mcp-ui/src/client/App.tsx
+++ projects/ramen-mcp-ui/src/client/App.tsx
@@ -1,31 +1,4 @@
-import React, { useState } from 'react'
-import { UIResourceRenderer } from '@mcp-ui/client'
-import { Client } from '@modelcontextprotocol/sdk/client/index.js'
-import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp.js'
-import type { ContentBlock, Resource } from '@modelcontextprotocol/sdk/types.js'
-
-const fetchMcpResource = async (toolName: string): Promise<ContentBlock> => {
-  const client = new Client({
-    name: 'streamable-http-client',
-    version: '0.0.0'
-  })
-
-  const transport = new StreamableHTTPClientTransport(new URL('/mcp', window.location.href))
-  await client.connect(transport)
-  let result
-  if (toolName === 'dice_roll') {
-    result = await client.callTool({
-      name: toolName,
-      arguments: {
-        spots: 6
-      }
-    })
-  } else {
-    throw new Error(`Unknown tool: ${toolName}`)
-  }
-
-  return (result?.content as ContentBlock[])[0]
-}
+import React from 'react'
 
 const App: React.FC = () => {
   return (
